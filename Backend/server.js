77942730defe94b590988b0da877eb710f1aaf82
parvashah305 <parvashah305@gmail.com>
2024-10-22 16:54:56 +0530

    changes in backend

--- Backend/server.js
+++ Backend/server.js
@@ -57,7 +57,18 @@ const port = 3000;
 const app = express();
 
 app.use(bodyparser.json());
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', "https://lock-it-down-your-password-manager-parvashah.vercel.app");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
+
+app.use(cors({
+  origin: "https://lock-it-down-your-password-manager-parvashah.vercel.app", 
+  credentials: true
+}));
 
 
 const dbUrl = process.env.MONGO_URI;
